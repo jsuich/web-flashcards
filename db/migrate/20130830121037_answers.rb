76ea5305778a661d4925441df7114d19d3201856
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 15:09:37 -0500

    Fixed Migration Bug

--- db/migrate/20130830121037_answers.rb
+++ db/migrate/20130830121037_answers.rb
@@ -2,9 +2,11 @@ class Answers < ActiveRecord::Migration
 
   def change
 
-    change_table :answers do |t|
+    create_table :answers do |t|
+
       t.belongs_to :card
       t.belongs_to :round
+
       t.string :input
       t.boolean :correctness
 
