ac0c2d0dae0fd10c52228d9c3d22c285be2b3cd2
Apprentice <apprentice@dbc-chicago-009.local>
2013-09-01 10:22:02 -0500

    Debugging lost variable values

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -12,13 +12,16 @@ get '/round/:id/:card_id' do
   @card_array = @deck.cards
 
   if params[:card_id].nil?
-    @current_card_id = 1
-    Round.create(current_user.id, @deck.id)
+    @current_card_id = @card_array.first.id
+    @current_round = Round.create(current_user.id, @deck.id)
+
     erb :round
   elsif cards_left?#params[:card_id].to_i < @card_array.last.id
     @current_card_id = params[:card_id].to_i + 1
+
     erb :round
   else
+
     erb :results 
   end
 
--- app/helpers/current_user_helper.rb
+++ app/helpers/current_user_helper.rb
@@ -2,9 +2,9 @@ def current_user
   @current_user ||= User.find(session[:user_id])
 end
 
-def current_round
-  @current_round ||= Round.last#find_by(user_id: current_user)#.last
-end
+# def current_round
+#   @current_round ||= Round.where(user_id: current_user.id).last#find_by(user_id: current_user)#.last
+# end
 
 def cards_left?
   params[:card_id].to_i < @card_array.last.id
